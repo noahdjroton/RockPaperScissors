7d70e4c560ba5b64656b83c58f47ebe083d08bb8
Djroton Noé Sossou <70499809+noahdjroton@users.noreply.github.com>
2024-01-28 17:49:33 +0100

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Djroton Noé SOSSOU on 28/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
